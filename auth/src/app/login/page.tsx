008d574e407c6a28707bb04f296a684c90556d6f
thevaishndra <chandrav306m@gmail.com>
2025-03-01 17:13:53 +0530

    set up

---
+++ auth/src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage(){
+    return (
+        <div className="flex">
+            <h1 className="text-center text-white text-2xl" >Login</h1>
+        </div>
+    )
+}
+//we can also customize our 404 page -> read nextjs docs
