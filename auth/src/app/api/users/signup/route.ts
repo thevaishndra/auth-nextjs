19b554f68adcebd8b55c203bccdccd04fcad44fc
thevaishndra <chandrav306m@gmail.com>
2025-03-02 15:46:34 +0530

    user schema

---
+++ auth/src/app/api/users/signup/route.ts
@@ -0,0 +1,13 @@
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import {NextRequest, NextResponse } from"next/server"
+import bcryptjs from "bcryptjs";
+
+
+
+
+connect();
+
+export async function POST(request: NextRequest){
+    
+}
